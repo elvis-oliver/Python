7cd01b7f5d76b411a7bea320b6d658c85a28738f
elvis-oliver <elvismuse@gmail.com>
2020-09-01 21:54:20 -0300

    ex:18,19,20e21
    exercicios 18, 19, 20 e 21

---
+++ CursoemVideo/desafio18.py
@@ -0,0 +1,7 @@
+import math
+angulo = int(input('Digite o ângulo: '))
+rad = math.radians(angulo)
+print('Seu SENO é de: {:.2f}'.format(math.sin(rad)))
+print('Seu COSSENO é de: {:.2f}'.format(math.cos(rad)))
+print('Sua TANGENTE é de: {:.2f}'.format(math.tan(rad)))
+
