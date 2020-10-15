03627158eaea12011b239a6f41d662b62885b32e
elvis-oliver <69014721+elvis-oliver@users.noreply.github.com>
2020-10-14 22:31:57 -0300

    mudanças:a4,5,6,7

--- exercicios de python/A4_desafio03.py
+++ exercicios de python/A4_desafio03.py
@@ -1,4 +1,4 @@
-n1=input('Digite o primeiro número: ')
-n2=input('Digite o segundo número: ')
-s=n1+n2
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+s = n1+n2
 print('A soma entre eles é igual a:', s)
