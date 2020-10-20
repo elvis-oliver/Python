e8ce990d517ec6aa18645f16bca813c8a1a35aed
elvis-oliver <69014721+elvis-oliver@users.noreply.github.com>
2020-10-19 22:56:18 -0300

    mudanças: a8,9

--- CursoemVideo/desafio16aula08i.py
+++ CursoemVideo/desafio16aula08i.py
@@ -1,9 +1,9 @@
 '''
 MINHA SOLUÇÃO
-num = float(input('Digite um número: '))
+num = float(input('Digite um número real: '))
 print('A parte inteira do número {} é {}'.format(num, int(num)))
 '''
 
 from math import trunc
-num = float(input('Digite um número: '))
+num = float(input('Digite um número real: '))
 print('O número digitado foi {} e sua porção inteire é {}'.format(num, trunc(num)))
